c2e647e6d886a51e74fb6c6ae5459c9fb0ab0472
Pakochu <pacohung2000@gmail.com>
2021-03-17 22:09:13 +0800

    Create taxcalbest.py

---
+++ taxcalbest.py
@@ -0,0 +1,181 @@
+'''
+Pair programming project
+by Paco Hung
+3/3/2021
+'''
+def income():
+    try:
+        global h, w, seperate, joint
+        h = float(input("Your personal income: "))
+        w = float(input("Spouse's personal income: "))
+        seperate = ' no data'
+        joint = ' no data'
+    except:
+        print("Please input a number...")
+        income()
+
+def tax(type, x):
+    global xtax, xstandard, xflag
+
+    if type == 'seperate':
+        if x*0.05 <= 18000:
+            xmpf = x*0.05
+        else:
+            xmpf = 18000
+            
+        xnet = max(0, x-xmpf-132000)
+
+    elif type == 'joint':
+        if x*0.05 <= 36000:
+            xmpf = x*0.05
+        else:
+            xmpf = 36000
+            
+        xnet = max(0, x-xmpf-264000)
+
+    xstandard = (x-xmpf)*0.15
+
+    xtax = 0
+
+    if (xnet-50000) >= 0:
+        xtax += 50000*0.02
+        xnet -= 50000
+        if (xnet-50000) >= 0:
+            xtax += 50000*0.06
+            xnet -= 50000
+            if (xnet-50000) >= 0:
+                xtax += 50000*0.1
+                xnet -= 50000
+                if (xnet-50000) >= 0:
+                    xtax += 50000*0.14
+                    xnet -= 50000
+                    xtax += xnet*0.17
+                    xnet = 0
+                else:
+                    xtax += xnet*0.14
+                    xnet = 0
+            else:
+                xtax += xnet*0.1
+                xnet = 0
+        else:
+            xtax += xnet*0.06
+            xnet = 0
+    else:
+        xtax += xnet*0.02
+        xnet = 0
+
+    if xtax < xstandard:
+        xflag = 0
+        return xtax, xflag
+    elif xtax > xstandard:
+        xflag = 1
+        xtax = xstandard
+        return xtax, xflag
+
+
+def menu():
+    print(
+        '''
+====================================================================
+--------------------- Hong Kong Tax Calculator ---------------------
+====================================================================
+╔════════════════════════════════════════════╗
+    Current data:                  
+    Your income = HK${}/year  
+    Spouse's income = HK${}/year
+
+    Seperate assessment = HK${}
+    Joint assessment = HK${}
+╚════════════════════════════════════════════╝                               
+
+[1] Seperate
+
+[2] Joint
+
+[3] Recommend
+
+[4] Re-enter
+
+[0] Exit program
+        '''
+        .format(h, w, seperate, joint)
+    )
+
+if __name__ == '__main__':
+    try:
+        print("Welcome! To begin, please enter the following...\n")
+        income()
+        menu()
+        select = input("Selection: ")
+        
+        while select != 0:
+            if select == 1:
+                seperate = 0
+
+                tax('seperate', h)
+
+                if xflag == 1:
+                    print("Your tax payable: HK${} (At standard rate)".format(xtax))
+                else:
+                    print("Your tax payable: HK${}".format(xtax))
+
+                seperate += xtax
+
+                tax('seperate', w)
+
+                if xflag == 1:
+                    print("Spouse's tax payable: HK${} (At standard rate)".format(xtax))
+                else:
+                    print("Spouse's tax payable: HK${}".format(xtax))
+
+                seperate += xtax
+
+                print("Combined seperate: HK${}".format(seperate))
+
+                input("Press Enter to return to the menu...")
+
+            elif select == 2:
+                joint = 0
+
+                tax('joint', h+w)
+
+                joint += xtax
+
+                if xflag == 1:
+                    print("Joint tax payable: HK${} (At standard rate)".format(xtax))
+                else:
+                    print("Joint tax payable: HK${}".format(xtax))
+
+                input("Press Enter to return to the menu...")
+
+            elif select == 3:
+                if type(seperate) == str or type(joint) == str:
+                    print("Please calculate both seperate and joint assessments first")
+                else:
+                    print("Seperate assessment = HK${}".format(seperate))
+                    print("Joint assessment = HK${}".format(joint))
+
+                if seperate < joint:
+                    print("It is recommended that you select seperate assessment")
+                elif joint < seperate:
+                    print("It is recommended that you select joint assessment")
+                else:
+                    print("Both assessment methods are equal")
+
+                input("Press Enter to return to the menu...")
+            elif select == 4:
+                income()
+                input("Press Enter to return to the menu...")
+            elif select ==0:
+                break
+            else:
+                print ("Invalid input")
+
+            menu()
+            select = int(input("Selection: "))
+
+        print("Goodbye!")
+
+    except Exception as e:
+        print("Fatal error: ")
+        print(e)
